e9afc28f75b79255b7e8b6bcddced06cd11b0b28
andrewlapointe <andrewrlapointe@gmail.com>
2024-06-17 16:46:10 -0400

    added zoom and tooltips to t-test plot

--- scripts/statParaTest.js
+++ scripts/statParaTest.js
@@ -1,10 +1,7 @@
-// const SVGwidth = document.getElementById("chart").offsetWidth * 0.8;
-// const SVGheight = 2 * (SVGwidth / 3);
-
 const sectionID = "statParaTest";
 const dataRelPath = "data/statistical_parametric_test.csv";
-const yAxisLabelText = "xAxis";
-const xAxisLabelText = "yAxis";
+const yAxisLabelText = "yAxis";
+const xAxisLabelText = "xAxis";
 
 const xValue = (d) => d["FDR"];
 const yValue = (d) => d["-log10(p)"];
@@ -32,59 +29,100 @@ function createScatterPlot(
 ) {
   // set the dimensions and margins of the graph
   const width = containerWidth - margin.left - margin.right,
-    height = containerHeight - margin.top - margin.bottom;
-
-  var zoom = d3
-    .zoom()
-    .scaleExtent([1, 2000])
-    .translateExtent([
-      [0, 0],
-      [width, height],
-    ])
-    .on("zoom", zoomFunction);
-  function zoomFunction(d) {
-    var transform = d3.zoomTransform(this);
-    d3.selectAll(".dot")
-      .attr("transform", transform)
-      .attr("r", 3 / Math.sqrt(transform.k));
-    xLabel.call(xAxis.scale(transform.rescaleX(xScale)));
-    yLabel.call(yAxis.scale(transform.rescaleY(yScale)));
-    svg
-      .selectAll(".threshold")
-      .attr("transform", transform)
-      .attr("stroke-width", 1 / transform.k);
-    svg
-      .select(".x.grid")
-      .call(
-        make_x_gridlines()
-          .tickSize(-innerHeight)
-          .tickFormat("")
-          .scale(transform.rescaleX(xScale))
-      );
-  }
+    height = containerHeight - margin.top - margin.bottom,
+    x = d3.scaleLinear().domain([0, 1600]).range([0, width]),
+    y = d3.scaleLinear().domain([0, 20]).range([height, 0]);
 
   // append the svg object to the body of the page
-  const svg = d3
-    .select(`#${containerID}`)
-    .append("svg")
-    .attr("preserveAspectRatio", "xMinYMin meet")
-    .attr(
-      "viewBox",
-      `0 0 ${width + margin.left + margin.right} ${
-        height + margin.top + margin.bottom
-      }`
-    )
-    // .attr("width", width + margin.left + margin.right)
-    // .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  //Read the data
+  function circleClass(d) {
+    if (d.y <= 1) {
+      return "dot";
+    } else if (d.y > 1 && d.x <= -1) {
+      return "dot sigfold";
+    } else if (d.x > 1 && d.y >= 1) {
+      return "dot sig";
+    } else {
+      return "dot";
+    }
+  }
+
   const renderViz = async () => {
     const data = await d3.csv(dataFile, parser);
-    // console.log(data);
+
+    const zoom = d3
+      .zoom()
+      .scaleExtent([1, 20000])
+      .translateExtent([
+        [0, 0],
+        [width, height],
+      ])
+      .on("zoom", (event) => {
+        const transform = event.transform;
+        const rescaleX = transform.rescaleX(x);
+        const rescaleY = transform.rescaleY(y);
+        xAxis.call(d3.axisBottom(rescaleX));
+        yAxis.call(d3.axisLeft(rescaleY));
+        svg
+          .selectAll(".dot")
+          .attr("transform", transform)
+          .attr("r", 3 / (2 * Math.sqrt(transform.k)));
+      });
+
+    const svg = d3
+      .select(`#${containerID}`)
+      .append("svg")
+      .attr("preserveAspectRatio", "xMinYMin meet")
+      .attr(
+        "viewBox",
+        `0 0 ${width + margin.left + margin.right} ${
+          height + margin.top + margin.bottom
+        }`
+      )
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .call(zoom);
+
+    const zoomBox = svg
+      .append("rect")
+      .attr("class", "zoom")
+      .attr("height", innerHeight)
+      .attr("width", innerWidth);
+
+    const tooltip = d3
+      .select("body")
+      .append("div")
+      .attr("class", "tooltipvolcano");
+
+    function tipEnter(_, d) {
+      console.log(JSON.stringify(d));
+      tooltip
+        .style("visibility", "visible")
+        .style("font-size", "11px")
+        .html(
+          "<strong>Protein</strong>: " +
+            d.name +
+            "<br/>" +
+            "<strong>-log10(p)</strong>: " +
+            d.y +
+            "<br/>" +
+            "<strong>FDR</strong>: " +
+            d3.format(".2f")(d.x) +
+            "<br/>" +
+            "<strong>PVal</strong>: " +
+            d.pVal +
+            "<br/>" +
+            "<strong>t.stat</strong>: " +
+            d3.format(".2f")(d.tStat)
+        );
+    }
+    function tipMove(event) {
+      tooltip
+        .style("top", event.pageY - 5 + "px")
+        .style("left", event.pageX + 20 + "px");
+    }
+
     // Add X axis
-    const x = d3.scaleLinear().domain([0, 1600]).range([0, width]);
     const xAxis = svg
       .append("g")
       .attr("transform", "translate(0," + height + ")")
@@ -100,7 +138,6 @@ function createScatterPlot(
       .text(xAxisLabelText);
 
     // Add Y axis
-    var y = d3.scaleLinear().domain([0, 20]).range([height, 0]);
     const yAxis = svg.append("g").call(d3.axisLeft(y));
 
     // Add Y axis label
@@ -112,24 +149,19 @@ function createScatterPlot(
       .attr("fill", "black")
       .attr("text-anchor", "middle")
       .text(yAxisLabelText);
-    svg
-      .append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
-
-    // Add Y axis
-    var y = d3.scaleLinear().domain([0, 20]).range([height, 0]);
-    svg.append("g").call(d3.axisLeft(y));
 
     const marks = data.map((d) => ({
-      x: x(xValue(d)) * 10_000,
+      x: x(xValue(d) * 10_000),
       y: y(yValue(d)),
+      name: d[""],
+      pVal: d["p.value"],
+      tStat: d["t.stat"],
     }));
-    console.log(marks);
 
     // Add dots
-    svg
+    const dots = svg
       .append("g")
+      .attr("class", "dots")
       .selectAll("dot")
       .data(marks)
       .enter()
@@ -137,8 +169,20 @@ function createScatterPlot(
       .attr("cx", (d) => d.x)
       .attr("cy", (d) => d.y)
       .attr("r", 1.5)
-      .style("fill", "#69b3a2");
+      .style("fill", "#69b3a2")
+      .attr("class", circleClass)
+      .on("mouseenter", tipEnter)
+      .on("mousemove", tipMove)
+      .on("mouseleave", () => tooltip.style("visibility", "hidden"))
+      .on(
+        "click",
+        (_, d) => window.open("https://salivaryproteome.org/protein/" + d.name),
+        "_blank"
+      );
+
+    // Add zoom
   };
+
   renderViz();
 }
 
